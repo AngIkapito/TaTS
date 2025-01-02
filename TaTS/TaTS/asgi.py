eb1c1960c54cd66c9458946be4f96a55c14a43e0
joshua <joshuartiongco@gmail.com>
2025-01-02 19:14:13 +0800

    made a project named "tats" stands for "ticketing and tracking system for psa pampanga provincial"

--- TaTS/TaTS/asgi.py
+++ TaTS/TaTS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TaTS project.
+ASGI config for tats project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaTS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tats.settings')
 
 application = get_asgi_application()
--- TaTS/TaTS/wsgi.py
+++ TaTS/TaTS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TaTS project.
+WSGI config for tats project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaTS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tats.settings')
 
 application = get_wsgi_application()
